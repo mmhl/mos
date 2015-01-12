ad0778b35412bc0855aa62292c476f8b79fddbcd
mhl <mhl.mieczkowski@sent.com>
2015-01-12 22:57:07 +0100

    Loader assembly code for own cross_compiler

--- loader.s
+++ loader.s
@@ -1,17 +1,31 @@
-global loader 
-MBOOT_MAGIC_NUMBER equ 0x1BADB002
-MBOOT_FLAGS equ 0x0
-MBOOT_CHECKSUM equ -(MBOOT_MAGIC_NUMBER + MBOOT_FLAGS)
 
-section .text:
-        align 4
-        dd MBOOT_MAGIC_NUMBER
-        dd MBOOT_FLAGS
-        dd MBOOT_CHECKSUM
-        
-loader:
-        mov eax, 0xCAFEBABE
-.loop:
-        jmp .loop
+.set MULTIBOOT_FLAG_ALIGN, 1<<0
+.set MULTIBOOT_FLAG_MEMINFO, 1<<1
+.set MULTIBOOT_FLAGS, 0x0
+.set MULTIBOOT_MAGIC, 0x1BADB002
+.set MULTIBOOT_CHECKSUM, -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
 
 
+.section .mutliboot
+.align 4
+.long MULTIBOOT_MAGIC
+.long MULTIBOOT_FLAGS
+.long MULTIBOOT_CHECKSUM
+
+
+# aw - allocatable writable @nobits no data here
+.section .bootstrap_stack, "aw", @nobits 
+stack_bottom:
+        .skip 4096
+stack_top:
+
+
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+        movl $stack_top, %esp #setup stack
+        #call kmain # c-code entry point
+        cli
+        hlt
